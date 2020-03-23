6b570c3494876f94d007086d4e2ebdab241ccd0c
audunaell <62010741+audunaell@users.noreply.github.com>
2020-03-23 15:38:12 +0100

    En del

--- opp1lib.js
+++ opp1lib.js
@@ -2,30 +2,56 @@
 function isDateValid(theDate) {
     var ListInp = theDate.split('').join('');
     var collectnr = ListInp[0] + ListInp[1] + ListInp[3] + ListInp[4] + ListInp[6] + ListInp[7] + ListInp[8] + ListInp[9];
-    
+    var inputL = ListInp.length;
 
-    if (ListInp.length == 10 && ListInp[2] == '.' && ListInp[5] == '.' && isNaN(collectnr) == false) {
+    var InputLenght = checkInputLenght(inputL, collectnr);
+    var dividedTest = checkifdivided(ListInp[2], ListInp[5]);
+    var daysGet = ListInp[0] + ListInp[1];
+    var mntGet = ListInp[3] + ListInp[4];
+    var yearGet = ListInp[6] + ListInp[7] + ListInp[8] + ListInp[9];
 
-        var daysGet = ListInp[0] + ListInp[1];
-        var mntGet = ListInp[3] + ListInp[4];
-        var yearGet = ListInp[6] + ListInp[7] + ListInp[8] + ListInp[9];
+    var theDay = parseInt(daysGet);
+    var theMnt = parseInt(mntGet);
+    var theYear = parseInt(yearGet);
 
-        var theDay = parseInt(daysGet);
-        var theMnt = parseInt(mntGet);
-        var theYear = parseInt(yearGet);
+    var daycheck = checkday(theDay, theMnt, theYear);
+    var monthCheck = checkmnt(theMnt);
+    var yearCheck = checkYear(yearGet);
 
-        var yearCheck = isLeapYear(theYear);
-        return (theMnt == 2 && theDay == 29 && yearCheck == true || theDay > 0 && theDay < 32 && theMnt == 1 || theDay > 0 && theDay < 29 && theMnt == 2
-            || theDay > 0 && theDay < 32 && theMnt == 3 || theDay > 0 && theDay < 31 && theMnt == 4 || theDay > 0 && theDay < 32 && theMnt == 5
-            || theDay > 0 && theDay < 31 && theMnt == 6 || theDay > 0 && theDay < 32 && theMnt == 7 || theDay > 0 && theDay < 32 && theMnt == 8
-            || theDay > 0 && theDay < 31 && theMnt == 9 || theDay > 0 && theDay < 32 && theMnt == 10
-            || theDay > 0 && theDay < 31 && theMnt == 11 || theDay > 0 && theDay < 32 && theMnt == 12);
-    } else {
-        return false;
+    return (InputLenght == true && dividedTest == true && daycheck == true && monthCheck == true && yearCheck == true);
+
+}
+
+function checkInputLenght(inputLenght, checkifnumber) {
+    return (inputLenght == 10 && isNaN(checkifnumber) == false);
+}
+
+function checkifdivided(spot3, spot6) {
+    return (spot3 == '.' && spot6 == '.');
+}
+
+function checkday(day, month, year) {
+    var LeapyearCheck = isLeapYear(year);
+
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        return (day > 0 && day <= 31);
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return (day > 0 && day <= 30);
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return (day > 0 && day <= 30);
+    } else if (month == 2) {
+        return (day > 0 && day <= 29 && LeapyearCheck == true || day > 0 && day <= 28);
     }
 }
 
+function checkYear(year) {
+    return (year.length === 4 && year >= '0000' && year <= '9999');
+}
 
 function isLeapYear(year) {
     return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+function checkmnt(month) {
+    return (month > 0 && month <= 12);
 }
